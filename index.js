057f1cde91fa068b58c3406a4bd1a58a0e7f89a5
Mayank Soni <79640667+nottmayank@users.noreply.github.com>
2023-08-05 12:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const topgg = require('@top-gg/sdk');
 const { Client, Intents, MessageEmbed, WebhookClient } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const config = require("./config.js"); // Importing config file
+const config = require("./config.js"); 
 
-const app = express(); // Creating web server using express
+const app = express();
 
 const webhook = new topgg.Webhook(config.passward);
 
@@ -15,43 +15,33 @@ client.on('ready', () => {
 
 app.post('/webhook', webhook.listener(async vote => {
     const user = await client.api.users(vote.user).get().catch(() => null);
-    if (config.logs.type == 1) {
+    if (!user) throw new Error('[API ERROR] Cannot find user.');
+
+    const embed = new MessageEmbed()
+    .setTitle("New vote received")
+    .setURL(`https://discord.com/users/${vote.user}`)
+    .setDescription(`${user.username}#${user.discriminator} has voted me on Top.gg`)
+    .setColor("RANDOM");
+
+    switch (config.logs.type) {
+      case 1: {
         if (!config.logs.channel.id.length) throw new Error('[CONFIG ERROR] No channel ID was provided');
-        else {
-            const channel = client.channels.fetch(config.logs.channel.id);
-            if (!channel) throw new Error("[DISCORD API ERROR] Couldn't find given channel ID");
-            else {
-                const embed = new MessageEmbed()
-                .setTitle("New vote received")
-                .setURL(`https://discord.com/users/${vote.user}`)
-                .setDescription(`${user.username}#${user.discriminator} has voted me on Top.gg`)
-                .setColor("RANDOM");
+        const channel = client.channels.fetch(config.logs.channel.id);
+        if (!channel) throw new Error("[DISCORD API ERROR] Couldn't find given channel ID");
 
-                await client.channels.get(config.logs.channel.id).send({ embeds: [embed]});
-                return true;
-            }
-        }
-    }
-    if (config.logs.type == 2) {
+        await client.channels.get(config.logs.channel.id).send({ embeds: [embed]});
+      }
+      case 2:
         if (!config.logs.webhook.id.length || !config.logs.webhook.token.length) throw new Error('[CONFIG ERROR] Webhook "ID" or "TOKEN" was not provided');
-        else {
-            const embed = new MessageEmbed()
-            .setTitle("New vote received")
-            .setURL(`https://discord.com/users/${vote.user}`)
-            .setDescription(`${user.username}#${user.discriminator} has voted me on Top.gg`)
-            .setColor("RANDOM");
 
-            const webhookClient = new WebhookClient({ id: config.logs.webhook.id, token: config.logs.webhook.token });
+        const webhookClient = new WebhookClient({ id: config.logs.webhook.id, token: config.logs.webhook.token });
 
-            webhookClient.send({ embeds: [embed]});
-            return true;
-        }
-    }
-    else throw new Error('[CONFIG ERROR] Invalid "type" was provided');
+        return webhookClient.send({ embeds: [embed]});
+    } 
 }));
 
 app.get("*", function (req, res) {
-    res.send("<h1>This is not a website. Made by Frenzy#1320");
+    res.send("<h1>This is not a website. By: <a href='https://github.com/nottmayank'>sonimayank</a></h1>");
 });
 
 
@@ -60,6 +50,14 @@ var server = app.listen(config.port, function () {
     var port = server.address().port;
     
     console.log("App listening at http://%s:%s", host, port)
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error(error);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
 });
 
 client.login(config.token);
